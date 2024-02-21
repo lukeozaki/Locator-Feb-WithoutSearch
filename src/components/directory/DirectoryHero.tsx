32a64541dcca2c5e00d4fb2051f2bc6265fe54b5
lukeozaki <103615984+lukeozaki@users.noreply.github.com>
2024-02-22 00:36:05 +0900

    remove dir searchbar

--- src/components/directory/DirectoryHero.tsx
+++ src/components/directory/DirectoryHero.tsx
@@ -38,12 +38,6 @@ const DirectoryHeroLayout = (props: DirectoryHeroProps) => {
           {line1 && <div className="Heading Heading--sub mb-6">{line1}</div>}
           {line2 && <div className="Heading Heading--head">{line2}</div>}
         </h1>
-        {props.searchPath && (
-          <DirectorySearchBar
-            placeholder="Search by city and state or ZIP code"
-            searcherPath={props.searchPath}
-          />
-        )}
       </div>
     </ErrorBoundaryWithAnalytics>
   );
